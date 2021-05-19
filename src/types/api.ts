ee70f616abc1ead40683fa92e066306aaecafffd
Pablo Martínez <martipev2@hotmail.com>
2021-05-18 21:00:42 -0500

    refactor(router): sql query refinado

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,12 +1,26 @@
 
-type Distrito = {
-    distrito: number;
-  }
-
 export type GetDistritoByCPBody = {
   cp: string;
 }
 
 export type GetDistritoByCPResponse = {
-  data: Distrito[];
+  bancada_original: string,
+  nombre_diputade: string,
+  entidad: number,
+  num_entidad: number,
+  num_distrito: number,
+  distrito: number,
+  id_legislativo: number,
+  bancada_actual: string,
+  reeleccion: boolean,
+  reeleccion_suplente: boolean,
+  licencia: boolean,
+  licencia_deceso: boolean,
+  nombre_suplente: string,
+  foto: string,
+  genero: string,
+  nombre_entidad: string,
+  municipio: 12,
+  nombre_municipio: string,
+  seccion: 3756,
 }
